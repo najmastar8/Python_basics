42c3b488ea3597b487e55294d70e30ddb2f78c68
valin <najma.vali@emc.com>
2019-07-10 22:05:05 +0530

    Commit to generators
    Commit to generators

--- Generators.py
+++ Generators.py
@@ -41,3 +41,27 @@ next (g)
 #And the next number is... 13!
 #About to yield 2
 #About to yield 4
+
+########################################################################################################
+########################################################################################################
+#Generators resume execution
+#Can maintain state in local variables
+#Complex control flow
+#LAzy evaluation
+def take (count,iterable):
+    counter=0
+    for item in iterable:
+        if counter==count:
+            return
+        counter +=1
+        yield item
+
+def run_take():
+    items =[2,4,6,8,10]
+    for item in take(3,items):
+
+        print (item)
+
+#if __name__=='__main__':
+run_take()
+
